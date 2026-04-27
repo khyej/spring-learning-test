c751baf2507de2cbc9c473fbd3639d4dafc73a64
khyej <khyej.h@gmail.com>
2026-04-27 15:46:00 +0900

    feat: spring-jdbc-1 학습

--- spring-jdbc-1/initial/src/main/java/cholog/QueryingDAO.java
+++ spring-jdbc-1/initial/src/main/java/cholog/QueryingDAO.java
@@ -4,6 +4,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.net.ResponseCache;
 import java.util.List;
 
 @Repository
@@ -31,7 +32,8 @@ public class QueryingDAO {
      */
     public int count() {
         //TODO : customers 디비에 포함되어있는 row가 몇개인지 확인하는 기능 구현
-        return 0;
+        int rowCount = jdbcTemplate.queryForObject("select count(*) from customers", Integer.class);
+        return rowCount;
     }
 
     /**
@@ -39,7 +41,8 @@ public class QueryingDAO {
      */
     public String getLastName(Long id) {
         //TODO : 주어진 Id에 해당하는 customers의 lastName을 반환
-        return null;
+        String lastName = jdbcTemplate.queryForObject("select last_name from customers where id = ?", String.class, id);
+        return lastName;
     }
 
     /**
@@ -48,7 +51,16 @@ public class QueryingDAO {
     public Customer findCustomerById(Long id) {
         String sql = "select id, first_name, last_name from customers where id = ?";
         //TODO : 주어진 Id에 해당하는 customer를 객체로 반환
-        return null;
+        Customer customer = jdbcTemplate.queryForObject(sql,
+                (resultSet, rowNum) -> {
+                    Customer findCustomer = new Customer(
+                            resultSet.getLong("id"),
+                            resultSet.getString("first_name"),
+                            resultSet.getString("last_name")
+                    );
+                    return findCustomer;
+                }, id);
+        return customer;
     }
 
     /**
@@ -57,7 +69,16 @@ public class QueryingDAO {
     public List<Customer> findAllCustomers() {
         String sql = "select id, first_name, last_name from customers";
         //TODO : 저장된 모든 Customers를 list형태로 반환
-        return null;
+        List<Customer> customers = jdbcTemplate.query(sql,
+                (resultSet, rowNum) -> {
+                    Customer customer = new Customer(
+                            resultSet.getLong("id"),
+                            resultSet.getString("first_name"),
+                            resultSet.getString("last_name")
+                    );
+                    return customer;
+                });
+        return customers;
     }
 
     /**
@@ -66,6 +87,17 @@ public class QueryingDAO {
     public List<Customer> findCustomerByFirstName(String firstName) {
         String sql = "select id, first_name, last_name from customers where first_name = ?";
         //TODO : firstName을 기준으로 customer를 list형태로 반환
-        return null;
+
+        List<Customer> customers = jdbcTemplate.query(sql,
+                (resultSet, rowNum) -> {
+                      Customer customer = new Customer(
+                              resultSet.getLong("id"),
+                              resultSet.getString("first_name"),
+                              resultSet.getString("last_name")
+                      );
+                      return customer;
+                }
+        , firstName);
+        return customers;
     }
 }
--- spring-jdbc-1/initial/src/main/java/cholog/UpdatingDAO.java
+++ spring-jdbc-1/initial/src/main/java/cholog/UpdatingDAO.java
@@ -33,13 +33,15 @@ public class UpdatingDAO {
      */
     public void insert(Customer customer) {
         //todo: customer를 디비에 저장하기
+        jdbcTemplate.update("insert into customers(first_name, last_name) values (?,?)", customer.getFirstName(), customer.getLastName());
     }
     /**
      * public int update(String sql, @Nullable Object... args)
      */
     public int delete(Long id) {
         //todo: id에 해당하는 customer를 지우고, 해당 쿼리에 영향받는 row 수반환하기
-        return 0;
+        int rowNum = jdbcTemplate.update("delete from customers where id = ?", id);
+        return rowNum;
     }
 
     /**
@@ -50,9 +52,14 @@ public class UpdatingDAO {
         KeyHolder keyHolder = new GeneratedKeyHolder();
 
         //todo : keyHolder에 대해 학습하고, Customer를 저장후 저장된 Customer의 id를 반환하기
-
+        jdbcTemplate.update(connection -> {
+            PreparedStatement psmt = connection.prepareStatement(sql, new String[]{"id"});
+            psmt.setString(1, customer.getFirstName());
+            psmt.setString(2, customer.getLastName());
+            return psmt;
+        }, keyHolder);
         Long id = keyHolder.getKey().longValue();
 
-        return keyHolder.getKey().longValue();
+        return id;
     }
 }
